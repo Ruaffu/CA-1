50c1aa29c35926267c6e42c1c4aa3480c289bf1d
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2022-03-15 09:52:38 +0100

    filter added to application config

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.PersonResource.class);
         resources.add(rest.HobbyResource.class);
         resources.add(rest.CityInfoResource.class);
+        resources.add(rest.CorsFilter.class);
         resources.add(errorhandling.PersonNotFoundExceptionMapper.class);
         resources.add(errorhandling.CityNotFoundExceptionMapper.class);
         resources.add(errorhandling.HobbyNotFoundExceptionMapper.class);
